db6d7fc4f63c72f3ba2bbfcb4483604a83beead8
KJWGarden <mike03131313@gmail.com>
2025-04-18 01:53:20 +0900

    Fix:CSS 수정

--- components/teamChart.tsx
+++ components/teamChart.tsx
@@ -21,43 +21,45 @@ export default function TeamChart({ teamName }: TeamChartProps) {
 
   return (
     <motion.div
-      initial={{ opacity: 0, x: 50 }}
+      initial={{ opacity: 0, x: -50 }}
       animate={{ opacity: 1, x: 0 }}
       transition={{
-        duration: 0.8,
+        duration: 1,
         delay: 0.3,
         ease: [0, 0.71, 0.2, 1.01],
       }}
+      className="flex justify-center"
     >
-      <div>
-        <p>{teamName} 갓같가 현황</p>
-      </div>
-      <div className="w-full h-full overflow-y-scroll">
-        <ChartContainer config={chartConfig} className="min-h-[200px] w-full">
-          <BarChart layout="vertical" accessibilityLayer data={filterdata}>
-            <CartesianGrid vertical={true} horizontal={false} />
-            <XAxis
-              type="number" // ✅ X축은 숫자형
-              domain={[0, 100]}
-              tickLine={false}
-              tickMargin={10}
-              axisLine={false}
-            />
-            <YAxis
-              type="category" // ✅ Y축은 카테고리형
-              dataKey="name" // ✅ 이름으로 라벨링
-              tickLine={false}
-              axisLine={false}
-            />
-            <Tooltip content={<CustomTooltip />} />{" "}
-            {/* (선택) 마우스 오버 시 데이터 표시 */}
-            <Bar
-              dataKey="filledHours"
-              fill="var(--color-filledHours)"
-              radius={4}
-            />
-          </BarChart>
-        </ChartContainer>
+      <div className="w-full lg:max-w-[60vw]">
+        <div>
+          <p>{teamName} 갓같가 현황</p>
+        </div>
+        <div className="w-full h-full overflow-y-scroll">
+          <ChartContainer config={chartConfig} className="min-h-[200px] w-full">
+            <BarChart layout="vertical" accessibilityLayer data={filterdata}>
+              <CartesianGrid vertical={true} horizontal={false} />
+              <XAxis
+                type="number"
+                domain={[0, 100]}
+                tickLine={false}
+                tickMargin={10}
+                axisLine={false}
+              />
+              <YAxis
+                type="category"
+                dataKey="name"
+                tickLine={false}
+                axisLine={false}
+              />
+              <Tooltip content={<CustomTooltip />} />
+              <Bar
+                dataKey="filledHours"
+                fill="var(--color-filledHours)"
+                radius={4}
+              />
+            </BarChart>
+          </ChartContainer>
+        </div>
       </div>
     </motion.div>
   );
--- components/toprating.tsx
+++ components/toprating.tsx
@@ -22,8 +22,9 @@ export default function Toprating() {
         delay: 0.5,
         ease: [0, 0.71, 0.2, 1.01],
       }}
+      className="flex flex-col items-center"
     >
-      <div className="pb-4">
+      <div className="pb-4 flex justify-center">
         <Image
           src="/menu.png"
           alt="메뉴사진"
@@ -40,35 +41,35 @@ export default function Toprating() {
             alt="확대된 메뉴 사진"
             width={800}
             height={600}
-            className="rounded-lg"
           />
         </DialogContent>
       </Dialog>
-
-      <div className="text-lg font-bold">Top 10</div>
-      <p className="text-xs">현재 시간을 가장 많이 채운 10명입니다!</p>
-      <div>
-        {top10.map((student, index) => (
-          <div key={student.id}>
-            <div className="flex pt-2 pb-1">
-              <div className="w-full">
-                <div className="flex gap-4 items-center">
-                  {index + 1}위
-                  <p className="text-xs font-light border rounded-lg px-1 bg-stone-200">
-                    {student.filledHours} 시간
-                  </p>
-                </div>
-                <div className="flex justify-between">
-                  <p>
-                    {student.name} {student.level}
-                  </p>
-                  <p>{student.department}</p>
+      <div className="flex flex-col lg:w-[40vw]">
+        <div className="text-lg font-bold">Top 10</div>
+        <p className="text-xs">현재 시간을 가장 많이 채운 10명입니다!</p>
+        <div>
+          {top10.map((student, index) => (
+            <div key={student.id}>
+              <div className="flex pt-2 pb-1">
+                <div className="w-full">
+                  <div className="flex gap-4 items-center">
+                    {index + 1}위
+                    <p className="text-xs font-light border rounded-lg px-1 bg-stone-200">
+                      {student.filledHours} 시간
+                    </p>
+                  </div>
+                  <div className="flex justify-between">
+                    <p>
+                      {student.name} {student.level}
+                    </p>
+                    <p>{student.department}</p>
+                  </div>
                 </div>
               </div>
+              <Separator orientation="horizontal" />
             </div>
-            <Separator orientation="horizontal" />
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </motion.div>
   );
